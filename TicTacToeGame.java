17d9be35e7dee749dad35efec685ebdb36f1291d
unknown <nituldeori63@gmail.com>
2020-11-24 10:44:15 +0530

    Added code to display current board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,6 +1,7 @@
 package com.bl.tictactoe;
 import java.util.Scanner;
 public class TicTacToeGame {
+	
 	Scanner sc=new Scanner(System.in);
 	public char[] board=new char[10];
 	public char playerChoice;
@@ -8,14 +9,18 @@ public class TicTacToeGame {
 	
 	// Create an empty tictactoe board
 	public char[] createBoard() {
+		
 		for(int i=1;i<board.length;i++) {
 			board[i]=' ';
 		}
+		
 		System.out.println("Created the board");
 		System.out.println(board);
 		return board;
 	}
+	//
 	public void makeChoice() {
+		
 		System.out.println("Enter your choice(O/X): ");
 		char Choice=sc.next().charAt(0);
 		if(Choice=='X') {
@@ -27,13 +32,29 @@ public class TicTacToeGame {
 		playerChoice=Choice;
 		
 	}
+	
+	// Display the current position of board
+	public void displayBoard() {
+		
+		int charInd=1;
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<3;j++) {
+				System.out.print("___"+"  "+board[charInd]);
+				charInd++;
+			}
+			System.out.println(" ");
+		}
+	}
+	
 	public static void main(String[] args) {
+		
 		System.out.println("******** Welcome to TicTacToeGame **********");
 		TicTacToeGame obj=new TicTacToeGame();
 		obj.createBoard();
 		obj.makeChoice();
 		System.out.println("Player choice: "+obj.playerChoice);
 		System.out.println("Computer choice: "+obj.computerChoice);
+		obj.displayBoard();
 	}
 
 }
